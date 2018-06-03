9a59bdd31f4542cde67a7c9446effb5fc2fef1ad
Nancina <nancinaj@yahoo.com>
2018-06-03 04:45:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ function shout(string){return string.toUpperCase()}
 function whisper(string){return string.toLowerCase()}
 function logShout(string){console.log(string.toUpperCase())}
 function logWhisper(string){console.log(string.toLowerCase())}
-function sayHiToGrandma(string){var lowerCase = string.toLowerCase();
+function sayHiToGrandma(string){if (string === string.toLowerCase()){var lowerCase = string.toLowerCase();
+return ('I can\'t hear you!');}
